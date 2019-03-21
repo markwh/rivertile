5fb5e6d354644def4487c530b3c96f5d66c7d05a
markwh <mark.hagemann@gmail.com>
2019-03-20 22:12:38 -0400

    added documentation, coverage function

--- R/validate.R
+++ R/validate.R
@@ -3,17 +3,24 @@
 
 #' Create validation data from two node-level rivertile data.frames.
 #'
+#' Used internally by \code{rt_valdata()}
+#'
+#' @param obs data.frame with observed node-level values
+#' @param truth data.frame with node-level truth values
+#' @param time_round_digits how many digits to round time (secondes) to
+#'  consider equal between gdem pixc and "real" pixc
+#'
 #' @export
 rt_valdata_df <- function(obs, truth, time_round_digits = -2) {
   # ID variables for joining rivertile to gdem
   idvars <- c("reach_id", "node_id", "time", "time_tai")
   idvars <- intersect(names(obs), idvars)
-  
+
   # time variables need to be rounded.
   timevars <- intersect(c("time", "time_tai"), names(obs))
   obs[timevars] <- round(obs[timevars], digits = time_round_digits)
   truth[timevars] <- round(truth[timevars], digits = time_round_digits)
-  
+
   # variables assumed constant between rivertile and gdem, can be joined
   # separately. Or, variables only meaningful for actual rivertile data
   commonvars_rch <- c(
@@ -28,7 +35,7 @@ rt_valdata_df <- function(obs, truth, time_round_digits = -2) {
     "iono_c", "xover_cal_c", "p_dist_out"
   )
   commonvars <- intersect(names(obs), c(commonvars_rch, commonvars_nod))
-  
+
   # Vector of variables to compare between rivertile and gdem
   varnames <- c("height", "height2", "slope", "width", "area_detct", "area_total",
                 "latitude", "longitude")
@@ -38,10 +45,10 @@ rt_valdata_df <- function(obs, truth, time_round_digits = -2) {
       "latitude_u", "longitud_u"),
     varnames
   )
-  
+
   varnames <- intersect(names(obs), varnames)
   uncnames <- uncnames[varnames]
-  
+
   # Make gathered data.frames
   obs_g <- gather(obs[c(idvars, varnames)],
                   key = "variable", value = "pixc_val", -!!idvars)
@@ -50,7 +57,7 @@ rt_valdata_df <- function(obs, truth, time_round_digits = -2) {
   uncdf_g <- obs[c(idvars, uncnames)] %>%
     setNames(plyr::mapvalues(names(.), from = uncnames, to = varnames)) %>%
     gather(key = "variable", value = "sigma_est", -!!idvars)
-  
+
   # Join together, including "common" variables
   commondf <- obs[c(idvars, commonvars)]
   out <- obs_g %>%
@@ -78,31 +85,68 @@ rt_valdata <- function(dir, group = c("nodes", "reaches"),
                        keep_na_vars = FALSE,
                        time_round_digits = -2,
                        flag_out_nodes = TRUE) {
-  
+
   rmnodes <- numeric(0)
   if (flag_out_nodes) {
     rmnodes <- flag_nodes(dir)
   }
-  
+
   group <- match.arg(group)
   rtdf <- rt_read(paste0(dir, "/", rtname), group = group,
                   keep_na_vars = keep_na_vars)
   gddf <- rt_read(paste0(dir, "/", gdname), group = group,
                   keep_na_vars = keep_na_vars)
-  
+
   out <- rt_valdata_df(obs = rtdf, truth = gddf, time_round_digits = -2)
   out <- out[!out[["node_id"]] %in% rmnodes, ]
   out
 }
 
+#' Flag nodes with questionable / unstable gdem truth
+#'
+#' Based on differences in "dilated" and non-dilated gdem.
+#'
+#' @param dir Directory containing gdem rivertile netcdfs
+#' @param gdem1,gdem2 gdem rivertile netcdfs to compare
+#' @param thresh Threshold for relative difference in widths, defaults to 0.15.
+#'
+#' @export
 flag_nodes <- function(dir, gdem1 = "rt_gdem.nc", gdem2 = "rt_gdem_dil2.nc",
                        thresh = 0.15) {
   valdata <- rt_valdata(dir, group = "nodes", rtname = gdem1, gdname = gdem2,
                         keep_na_vars = TRUE, flag_out_nodes = FALSE) %>%
-    select(reach_id, node_id, variable, pixc_val, gdem_val, pixc_err) %>%
-    filter(variable == "width") %>%
-    mutate(pct_diff = - pixc_err / gdem_val)
-  
+    dplyr::select(reach_id, node_id, variable, pixc_val, gdem_val, pixc_err) %>%
+    dplyr::filter(variable == "width") %>%
+    dplyr::mutate(pct_diff = - pixc_err / gdem_val)
+
   out <- with(valdata, node_id[pct_diff > thresh])
   out
 }
+
+
+#' Table of coverage for various (normal) confidence intervals.
+#'
+#' @param valdata as returned by \code{rt_valdata()}
+#' @param ci Which confidence levels to validate?
+#'
+#' @export
+val_coverage <- function(valdata, ci = c(68, 90, 95, 99), debias = FALSE) {
+  covfun <- function(x, sigma, pctl) {
+    if (debias) x <- x - mean(x, na.rm = TRUE)
+    pctl <- pctl / 100
+    bnd <- -qnorm((1 - pctl) / 2, mean = 0, sd = 1)
+    numin <- vapply(bnd, function(y) sum(abs(x / sigma) <= y), numeric(1))
+    out <- numin / length(x) * 100
+    out
+  }
+
+  vallist <- split(valdata, f = valdata$variable)
+  out <- purrr::map(vallist, ~covfun(x = .$pixc_err, sigma = .$sigma_est, pctl = ci)) %>%
+    as.data.frame() %>%
+    t() %>%
+    as.data.frame() %>%
+    setNames(paste0("ci", ci))
+
+  out
+}
+
