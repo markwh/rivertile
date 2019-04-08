1e06519c20483cfea9a0f4e8798eeaa91487238f
markwh <mark.hagemann@gmail.com>
2019-04-08 11:52:45 -0400

    fixed 2-tail bug in hypothesis test, made debias default to FALSE

--- R/hyp-test.R
+++ R/hyp-test.R
@@ -3,10 +3,12 @@
 #' Test uncertainty estimates against chi-square distribution
 #'
 #' @param valdata A data.frame as returned by \code{rt_valdata()}
-#' @param debias Remove effect of bias? Defaults to TRUE
+#' @param debias Remove effect of bias? Defaults to FALSE
+#' @param sides 1 for 1-sided, or 2 for 2-sided hypothesis test. If 1, test is on upper tail.
+#' @param log.p Report p-value as log-transformed value?
 #' @importFrom dplyr group_by mutate summarize
 #' @export
-rt_hyptest <- function(valdata, debias = TRUE) {
+rt_hyptest <- function(valdata, debias = FALSE, sides = 2, log.p = FALSE) {
   out <- valdata %>%
     mutate(relerr = pixc_err / sigma_est) %>%
     group_by(variable) %>%
@@ -16,8 +18,14 @@ rt_hyptest <- function(valdata, debias = TRUE) {
 
   out <- summarize(out,
                    teststat = sum((relerr - meanrelerr)^2, na.rm = TRUE),
-                   df = sum(!is.na(relerr)) - 1) %>%
-    mutate(pval = 2 * (1 - pchisq(teststat, df = df)))
+                   df = sum(!is.na(relerr)) - 1)
+  logpval <- pchisq(out$teststat, df = out$df, log.p = TRUE, lower.tail = FALSE)
+  if (sides == 2) {
+    logpval <- log(2) + ifelse(logpval > log(0.5), log(1 - exp(logpval)), logpval)
+  }
+
+  out$pval <- if (log.p) logpval else exp(logpval)
+
   out
 }
 
