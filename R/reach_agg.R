d2f9b15b3abc630f13e580a45623059831c6b1e1
Mark Hagemann <mark.hagemann@gmail.com>
2019-09-04 12:28:29 -0400

    Update reach aggregation to more safely handle loc_offset adjustment

--- R/reach_agg.R
+++ R/reach_agg.R
@@ -10,18 +10,19 @@
 #' @param node_h Vector of node heights
 #' @param node_h_u Vector of node height uncertainty (1-sigma)
 #' @param node_x Vector of node downstream distance
+#' @param x_ctr center of reach, as given by meandist in \code{add_offset()}
 #' @param loc_offset From reach-level rivertile, distance from observed to prior
 #'  reach center
 #' @param weight Use weighted least-squares? Default is TRUE.
 #' @importFrom stats lm
 #' @export
-reach_height_lm <- function(node_h, node_h_u, node_x, loc_offset,
+reach_height_lm <- function(node_h, node_h_u, node_x, x_ctr, loc_offset,
                             weight = TRUE) {
   # browser()
   weights <- 1 / (node_h_u^2)
   if (!weight) weights <- NULL
 
-  hxdf <- data.frame(x = node_x - mean(node_x) + loc_offset, h = node_h)
+  hxdf <- data.frame(x = node_x - x_ctr + loc_offset, h = node_h)
   hxmod <- lm(h ~ x, hxdf, weights = weights)
 
   hxmod
@@ -51,10 +52,18 @@ reach_agg <- function(nodedata, usenodes = "all", weight = TRUE) {
     nodedata <- dplyr::filter(nodedata, node_id %in% usenodes)
   }
 
+  # Make sure sufficient number of nodes are present per reach
+  usereaches <- reaches_tokeep(nodedata, minnodes = 3)
+  warn_reaches(nodedata, usereaches)
+  nodedata <- dplyr::filter(nodedata, reach_id %in% usereaches)
+  if (nrow(nodedata) == 0) return(NULL)
+
   # Make linear models for height, slope
   hxmods <- split(nodedata, f = nodedata$reach_id) %>%
     purrr::map(~reach_height_lm(node_h = .$wse, node_h_u = .$wse_r_u,
-                                node_x = .$cumlen, loc_offset = .$loc_offset,
+                                node_x = .$cumlen,
+                                x_ctr = .$meandist,
+                                loc_offset = .$loc_offset,
                                 weight = weight))
   hxcoef <- map(hxmods, ~as.data.frame(summary(.)$coefficients,
                                        row.names = FALSE)) %>%
@@ -65,8 +74,8 @@ reach_agg <- function(nodedata, usenodes = "all", weight = TRUE) {
   #  values and uncertainties come from param estimates and standard errors
   reach_heights <- dplyr::filter(hxcoef, param == "intercept")$est
   reach_heights_u <- dplyr::filter(hxcoef, param == "intercept")$std
-  reach_slopes <- dplyr::filter(hxcoef, param == "slope")$est * 1e6
-  reach_slopes_u <- dplyr::filter(hxcoef, param == "slope")$std * 1e6
+  reach_slopes <- -dplyr::filter(hxcoef, param == "slope")$est
+  reach_slopes_u <- dplyr::filter(hxcoef, param == "slope")$std
 
   nd_agg <- nodedata %>%
     group_by(.data$reach_id) %>%
@@ -84,6 +93,26 @@ reach_agg <- function(nodedata, usenodes = "all", weight = TRUE) {
 
   nd_agg
 }
+
+reaches_tokeep <- function(nodedata, minnodes = 3) {
+  if (nrow(nodedata) == 0) {
+    return(numeric(0))
+  }
+  nperdf <- nodedata %>%
+    group_by(reach_id) %>%
+    summarize(n = n())
+  out <- nperdf$reach_id[nperdf$n > minnodes]
+  out
+}
+
+warn_reaches <- function(nodedata, usereaches) {
+  badreaches <- setdiff(nodedata$reach_id, usereaches)
+  if (length(badreaches))
+    warning("The following reaches had insufficient data and were ",
+            "removed prior to aggregating: ",
+            badreaches)
+}
+
 
 
 #' Check whether all node IDs are sequential --i.e. none are missing.
@@ -104,9 +133,33 @@ all_sequential <- function(node_ids) {
 add_offset <- function(nodedata, reachdata) {
   reachadjdf <- reachdata %>%
     dplyr::select(reach_id, loc_offset)
+
+  # check for node distance
+  if (is.null(nodedata$cumlen)) stop ("nodedata must have length data (add_nodelen())")
+
+  nnodedf <- reachdata %>%
+    dplyr::select(reach_id, n_good_nod)
+
+  meandistdf <- nodedata %>%
+    group_by(reach_id) %>%
+    summarize(n = n(), meandist = mean(cumlen)) %>%
+    left_join(nnodedf, by = "reach_id")
+
+  # Check number of nodes is same used in riverobs
+  badnreaches <- meandistdf$n != meandistdf$n_good_nod
+  if (sum(badnreaches) > 0) {
+    warning("The following reaches have the wrong number of nodes:\n",
+            paste(meandistdf$reach_id[badnreaches], collapse = ", "), "\n",
+            paste(meandistdf$n[badnreaches], collapse = ", "),
+            " Should be: \n",
+            paste(meandistdf$n_good_nod[badnreaches]),
+            "\nDistances may be wrong.")
+  }
+
   # join to loc_offset from reachdata
   out <- nodedata %>%
-    left_join(reachadjdf, by = "reach_id")
+    left_join(reachadjdf, by = "reach_id") %>%
+    left_join(meandistdf[c("reach_id", "meandist")], by = "reach_id")
 
   out
 }
@@ -125,23 +178,28 @@ add_nodelen <- function(nodedata, force = FALSE) {
 
   out <- nodedata %>%
     arrange(node_id) %>%
+    group_by(reach_id) %>%
     mutate(nodelen = area_total / width,
-           cumlen = cumsum(nodelen))
+           cumlen = cumsum(nodelen),
+           meanlen = mean(nodelen)) %>%
+    ungroup()
   if (force) { # attempt to estimate cumlen.
     allnodes <- seq(min(nodeids), max(nodeids))
-    missingnodes <- setdiff(allnodes, out$node_id)
+    missingnodes <- sort(setdiff(allnodes, out$node_id))
 
-    nextinds <- missingnodes %>%
-      map_int(~which(out$node_id == min(out$node_id[out$node_id > .])))
     meandist <- mean(out$nodelen)
 
     for (i in seq_along(missingnodes)) {
       nodeidi <- missingnodes[i]
+      closestnodeind <- which.min(abs(nodeidi - out$node_id)) # TODO: fix this hack
+      meanleni <- out[closestnodeind, ]$meanlen
+
       gtidinds <- which(out$node_id > nodeidi)
 
-      out$cumlen[gtidinds] <- out$cumlen[gtidinds] + meandist
+      out$cumlen[gtidinds] <- out$cumlen[gtidinds] + meanleni
     }
   }
+  out$meanlen <- NULL
 
   out
 }
